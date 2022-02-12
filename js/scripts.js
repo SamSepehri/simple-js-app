cfa692b1ed88e26ef935431caa5e2872307dca85
samisep <52745072+samisep@users.noreply.github.com>
2022-02-12 15:45:34 +0100

    Update scripts.js
    Adding loop with for
    adding some conditions

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,27 @@
-let pokemonList = [{name: "Bulbasaur", type: "grass", height: "7"},{name: "Squirtle", type: "water", height: "5"},{name: "Charmander", type: "fire", height: "6"}];
+alert('Hello world');
+
+let pokemonList = [
+  {
+    name: "Bulbasaur",
+    type: ["grass","poison"],
+    height: 0.7
+  },
+  {
+    name: "Squirtle",
+    type: ["water"],
+    height: 1.7
+  },
+  {
+    name: "Charmander",
+    type: ["fire","flying"],
+    height: 0.6
+  }
+];
+
+for (let i=0; i < pokemonList.length; i++){
+  if(pokemonList[i].height > 1){
+    document.write(pokemonList[i].name + "( height : " + pokemonList[i].height + " )" + "Wow it`s a big pokemon!!!")
+  }else {
+  document.write(pokemonList[i].name + "( height : " + pokemonList[i].height + " )")
+}
+}
