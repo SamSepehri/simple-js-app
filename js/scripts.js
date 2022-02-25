324b9f25aec630e9eaa021cf9b3f9af77d796fa2
samisep <52745072+samisep@users.noreply.github.com>
2022-02-25 20:10:18 +0100

    Update scripts.js
    commenting my js data and
    add modal for each pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
+//Start of IIFE
 let pokemonRepository = (function () {
   let pokemonList = [];
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
+//add new pokemon to pokemonList
   function add(pokemon) {
     if (
       typeof pokemon === "object" &&
@@ -12,9 +14,12 @@ let pokemonRepository = (function () {
       console.log("pokemon is not correct");
     }
   }
+//return all pokemons
   function getAll() {
     return pokemonList;
   }
+
+//create button of pokemons
   function addListItem(pokemon){
     let pokemonList = document.querySelector(".pokemon-list");
     let listpokemon = document.createElement("li");
@@ -25,9 +30,8 @@ let pokemonRepository = (function () {
     pokemonList.appendChild(listpokemon);
     button.addEventListener("click", function(event) {
       showDetails(pokemon);
-    });
   }
-
+  //load list of pokemon from apiUrl
   function loadList() {
       return fetch(apiUrl).then(function (response) {
         return response.json();
@@ -44,7 +48,7 @@ let pokemonRepository = (function () {
         console.error(e);
       })
     }
-
+//load data of each pokemon when click on pokemon
     function loadDetails(item) {
       let url = item.detailsUrl;
       return fetch(url).then(function (response) {
@@ -58,12 +62,66 @@ let pokemonRepository = (function () {
         console.error(e);
       });
     }
-
-    function showDetails(item) {
-      pokemonRepository.loadDetails(item).then(function () {
-        console.log(item);
+//after click on pokemon button,load the data of pokemon from server
+    function showDetails(pokemon) {
+      pokemonRepository.loadDetails(pokemon).then(function () {
+        console.log(pokemon);
+        showModal(pokemon.name, pokemon.height, pokemon.imageUrl);
       });
     }
+
+    //Show a modal with pokemon details
+    function showModal(name, height, image){
+      console.log(`show modal function called`);// just for test
+      let modalContainer = document.querySelector('#modal-container');
+      console.log(`modalContainer: ${modalContainer}`);
+      modalContainer.innerHTML = '';
+      let modal = document.createElement('div');
+      modal.classList.add('modal');
+
+      let closeButtonElement = document.createElement('button');
+      closeButtonElement.classList.add('modal-close');
+      closeButtonElement.innerText = 'Close';
+      closeButtonElement.addEventListener('click', hideModal);
+
+      let titleElement = document.createElement('h1');
+      titleElement.innerText = name;
+
+      let contentElement = document.createElement('p');
+      contentElement.innerText = "Height:" + height;
+      //rendering an image of pokemon
+      let imageElement = document.createElement('img');
+      imageElement.classList.add('pokemon-image-class');
+      imageElement.src = image;
+
+      console.log(`imageElement.className is: ${imageElement.className}`);
+
+      modal.appendChild(closeButtonElement);
+      modal.appendChild(titleElement);
+      modal.appendChild(contentElement);
+      modal.appendChild(imageElement);
+      modalContainer.appendChild(modal);
+      modalContainer.classList.add('is-visible');
+    }
+
+    function hideModal(){
+      let modalContainer = document.querySelector('#modal-container');
+      modalContainer.classList.remove('is-visible');
+    }
+
+    let modalContainer = document.querySelector('#modal-container');
+    window.addEventListener('keydown', (e) =>{
+      if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+            hideModal();
+        }
+    });
+
+    modalContainer.addEventListener('click', (e) =>{
+      let target = e.target;
+      if (target ===modalContainer){
+        hideModal();
+      }
+    });
 
   return {
     add: add,
@@ -73,7 +131,7 @@ let pokemonRepository = (function () {
     loadList: loadList,
     loadDetails: loadDetails
   };
-})();
+})();//end of IIFE
 
 
 pokemonRepository.loadList().then(function() {
